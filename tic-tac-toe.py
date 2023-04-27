adf74611e2b939c3ab77af171a87f5cb6b23d921
TheAkashRoy <9akash.roy@gmail.com>
2023-04-28 02:39:21 +0530

    ai_move except minimax

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -21,18 +21,41 @@ def mark(board, x,y,sign):
     return
 
 def userInput(board):
-    move=int(input("Enter your move: "))
-    move-=1
-    movy=move%3
-    movx=move//3
+    flag=1
+    movy = 0
+    movx = 0
+    while(flag):
+        move=int(input("Enter your move: "))
+        move-=1
+        movy=move%3
+        movx=move//3
+        if board[movx][movy] != "-":
+            print("Oops! This place is already occupied")
+        else:
+            flag = 0
     mark(board,movx,movy,human) 
     return
 
-def ai_move(board):
-    pass
+def ai_move(board,ai):
+    best = -99
+    ij=[]
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == "-":
+                board[i][j] = ai
+                score= minimax(board)
+                board[i][j] = "-"
+                if score>best:
+                    best= score
+                    ij.append(i)
+                    ij.append(j)
+    final = []
+    final.append(ij[-2])
+    final.append(ij[-1])
+    return final
 
 def minimax(board):
-    pass
+    return 1
 
 board= [ ["-","-","-"],["-","-","-"],["-","-","-"] ]
 
@@ -46,7 +69,8 @@ while(spaceCount!=0 and win ==0):
     if spaceCount%2==1:
         userInput(board)
     else:
-        ai_move(board) #this will give the computer move
+        move = ai_move(board,ai) #this will give the computer move
+        board[move[0]][move[1]] = ai
     spaceCount-=1
     if isWin(board) == "None":
         printBoard(board)
